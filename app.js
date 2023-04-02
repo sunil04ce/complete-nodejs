37075f3c7b93816b72e9d1ff0c9fda58c62cd2b9
sunil parmar <sunil04ce@gmail.com>
2023-04-02 21:52:21 +0530

    use of process.argv

--- app.js
+++ app.js
@@ -7,11 +7,21 @@ const add = require('./utils.js');
 const getNotes = require('./notes.js');
 const chalk = require('chalk');
 
-console.log(add(3, 2));
-console.log(getNotes());
-console.log(validator.isEmail('jack@gmail.com'));
-console.log(validator.isURL('https://www.google.com'));
-console.log(chalk.bold.green('Success!'));
-console.log(chalk.green.inverse('Very good...'));
-console.log(chalk.yellow('Failed', chalk.underline.bgBlue('Try again')));
-console.log(chalk.black.bold.bgMagenta('Good Luck'));
+// console.log(add(3, 2));
+// console.log(getNotes());
+// console.log(validator.isEmail('jack@gmail.com'));
+// console.log(validator.isURL('https://www.google.com'));
+// console.log(chalk.bold.green('Success!'));
+// console.log(chalk.green.inverse('Very good...'));
+// console.log(chalk.yellow('Failed', chalk.underline.bgBlue('Try again')));
+// console.log(chalk.black.bold.bgMagenta('Good Luck'));
+
+console.log(process.argv);
+
+const command = process.argv[2];
+
+if (command === 'add') {
+    console.log('Adding note!');
+} else if (command === 'remove') {
+    console.log('Removing note!');
+}
