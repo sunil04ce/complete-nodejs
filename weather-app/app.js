80eab41f6fb6e60ccbd1b047cd5241999f75749c
sunil parmar <sunil04ce@gmail.com>
2023-04-10 23:01:45 +0530

    use of geocodeURL to get lat,long

--- weather-app/app.js
+++ weather-app/app.js
@@ -9,8 +9,15 @@ request({ url: url, json: true }, (error, response) => {
     if (response.body.success) {
         console.log(response.body.current.weather_descriptions[0] + ", There is " + response.body.current.temperature + " degrees temperature and fillslike temperature is " + response.body.current.feelslike + " degrees");
     }
+});
 
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
 
+request({ url: geocodeURL, json: true }, (error, response) => {
+    console.log(response.body);
+    const longtitude = response.body.features[0].center[0];
+    const latitude = response.body.features[0].center[1];
+    console.log(longtitude, latitude);
 });
 
 // console.log('Starting')
