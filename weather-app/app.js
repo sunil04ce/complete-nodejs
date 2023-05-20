1ad953c6822ad520de6b71e59a208dc93b599344
sunil parmar <sunil04ce@gmail.com>
2023-05-20 14:43:36 +0530

    error handling

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,24 +1,37 @@
 const request = require('request');
 
-const url = 'http://api.weatherstack.com/current?access_key=6af0218b9dcd82a572d8a9d42adf939a&query=23.1061874617057,%2072.54090099867204';
+const url = 'http://api.weatherstack.com/current?access_key=38111959c2b6a0c576cd949543624f11&query=New%20York';
 
 request({ url: url, json: true }, (error, response) => {
-    // const data = JSON.parse(response.body);
-    // console.log(data.current);
-    console.log(response.body);
-    if (response.body.success) {
+
+    if (error) {
+        console.log("Unable to connect weather service!");
+    } else if (response.body.error) {
+        console.log("Unable to find the location"); //(response.body.error.info);
+    } else {
+        // const data = JSON.parse(response.body);
+        // console.log(data.current);
+        console.log(response.body);
+        //if (response.body.success) {
         console.log(response.body.current.weather_descriptions[0] + ", There is " + response.body.current.temperature + " degrees temperature and fillslike temperature is " + response.body.current.feelslike + " degrees");
+        //}
     }
 });
 
 const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
 
-request({ url: geocodeURL, json: true }, (error, response) => {
-    console.log(response.body);
-    const longtitude = response.body.features[0].center[0];
-    const latitude = response.body.features[0].center[1];
-    console.log(longtitude, latitude);
-});
+// request({ url: geocodeURL, json: true }, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect location service!');
+//     } else if (response.body.features.length === 0) {
+//         console.log("Unable to find location, Try another search.")
+//     } else {
+//         console.log(response.body);
+//         const longtitude = response.body.features[0].center[0];
+//         const latitude = response.body.features[0].center[1];
+//         console.log(longtitude, latitude);
+//     }
+// });
 
 // console.log('Starting')
 
