67c716600765593d4dff3180f0237f8d43aa06b8
sunil parmar <sunil04ce@gmail.com>
2023-06-28 09:52:16 +0530

    use of Query string

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -62,9 +62,31 @@ app.get('/help', (req, res) => {
 });
 
 app.get('/weather', (req, res) => {
+
+    if (!req.query.address) {
+        return res.send({
+            error: "You must provide an address."
+        })
+    }
+
     res.send({
         forcast: "It is snowing",
-        location: 'New York'
+        location: 'New York',
+        address: req.query.address
+    });
+});
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: "You must provide a search term"
+        });
+    }
+
+    console.log(req.query);
+    console.log(req.query.search);
+    res.send({
+        products: []
     });
 });
 
