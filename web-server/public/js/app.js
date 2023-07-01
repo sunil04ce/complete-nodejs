74718b90cffcd3ba3a4aa58b828d915dc2a09996
sunil parmar <sunil04ce@gmail.com>
2023-07-01 16:50:20 +0530

    Browser HTTP request with fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,18 @@
 console.log('Client side javascript file is loaded.');
+
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data);
+//     });
+// });
+
+fetch('http://localhost:3000/weather?address=New%20York').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error);
+        } else {
+            console.log(data.forcast);
+            console.log(data.location);
+        }
+    });
+})
