77a8b05c14e97c5c5780c9d6d037c3ea60f4ffd3
sunil parmar <sunil04ce@gmail.com>
2023-07-01 17:52:13 +0530

    added search form, changes in styles

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -6,13 +6,31 @@ console.log('Client side javascript file is loaded.');
 //     });
 // });
 
-fetch('http://localhost:3000/weather?address=New%20York').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error);
-        } else {
-            console.log(data.forcast);
-            console.log(data.location);
-        }
+
+
+const weatherForm = document.querySelector('form');
+const searchInput = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const locationText = searchInput.value;
+
+    console.log("locationText -> " + locationText);
+
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
+
+    fetch('http://localhost:3000/weather?address=' + locationText).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error;
+            } else {
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forcast;
+            }
+        });
     });
-})
+
+});
