c84dcb7e517e96cfd276cd7ec2b9399980bd3351
sunil parmar <sunil04ce@gmail.com>
2023-07-07 22:53:02 +0530

    initial commit with mongodb

---
+++ task-manager/mongodb.js
@@ -0,0 +1,17 @@
+// CRUD 
+
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
+
+MongoClient.connect(connectionURL, {
+    useNewUrlParser: true
+}, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to Database!');
+    }
+
+    console.log('Connected successfully!');
+});
