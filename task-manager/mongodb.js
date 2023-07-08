34d246a74a7cc62cd7d548e2949419e223cdaf97
sunil parmar <sunil04ce@gmail.com>
2023-07-08 18:18:31 +0530

    mongodb connecting and inserting document

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -12,6 +12,11 @@ MongoClient.connect(connectionURL, {
     if (error) {
         return console.log('Unable to connect to Database!');
     }
-
     console.log('Connected successfully!');
+
+    const db = client.db(databaseName);
+    db.collection('users').insertOne({
+        name: 'Jack',
+        age: 27
+    });
 });
