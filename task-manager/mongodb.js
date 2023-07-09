cb94f5d88abfcb2eb4cf7916a7235dbbe333e2f0
sunil parmar <sunil04ce@gmail.com>
2023-07-09 13:08:55 +0530

    insertMany documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -15,8 +15,52 @@ MongoClient.connect(connectionURL, {
     console.log('Connected successfully!');
 
     const db = client.db(databaseName);
-    db.collection('users').insertOne({
-        name: 'Jack',
-        age: 27
+    // db.collection('users').insertOne({
+    //     name: 'Jack',
+    //     age: 27
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user');
+    //     }
+    //     console.log(result.ops);
+    // });
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jen',
+    //         age: 38
+    //     },
+    //     {
+    //         name: 'Gunther',
+    //         age: 30
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert documents!');
+    //     }
+    //     console.log(result.ops);
+
+    // });
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'Shop grocessary',
+            completed: false
+        },
+        {
+            description: 'Repair Bike',
+            completed: true
+        },
+        {
+            description: 'Clean parking',
+            completed: true
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log('Unable to insert tasks!');
+        }
+        console.log(result.ops);
     });
+
 });
+
