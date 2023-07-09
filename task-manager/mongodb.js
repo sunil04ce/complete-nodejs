4fe650dfb41f59994fc04a1b08cf1936f122da43
sunil parmar <sunil04ce@gmail.com>
2023-07-09 14:09:39 +0530

    use of ObjectID

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,10 +1,19 @@
 // CRUD 
 
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectID;
+
+const { MongoClient, ObjectID } = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
+
+const id = new ObjectID();
+console.log(id);
+console.log(id.id.length);
+console.log(id.toHexString().length);
+console.log(id.getTimestamp());
 
 MongoClient.connect(connectionURL, {
     useNewUrlParser: true
