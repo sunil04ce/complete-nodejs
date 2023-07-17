d8b64a90c8dcad3c42753c6760fbc0e505722097
sunil parmar <sunil04ce@gmail.com>
2023-07-17 09:41:13 +0530

    created get api endpoints using find, findById

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -21,6 +21,28 @@ app.post('/users', (req, res) => {
     });
 })
 
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((error) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    // console.log(req.params);
+    const _id = req.params.id;
+
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((error) => {
+        res.status(500).send()
+    })
+})
+
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body);
     task.save().then(() => {
@@ -28,6 +50,27 @@ app.post('/tasks', (req, res) => {
     }).catch((error) => {
         res.status(400).send(error)
     });
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((error) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id;
+
+    Task.findById(_id).then((task) => {
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    }).catch((error) => {
+        res.status(500).send()
+    })
 })
 
 app.listen(port, () => {
