89ccbd5906e6c9166e0f4ec0b6cde8c577d67833
sunil parmar <sunil04ce@gmail.com>
2023-07-21 23:03:02 +0530

    created delete user and task endpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -100,6 +100,18 @@ app.patch('/users/:id', async (req, res) => {
     }
 })
 
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    } catch (error) {
+        res.status(500).send();
+    }
+})
+
 // app.post('/tasks', (req, res) => {
 //     const task = new Task(req.body);
 //     task.save().then(() => {
@@ -184,6 +196,19 @@ app.patch('/tasks/:id', async (req, res) => {
         res.status(400).send(error);
     }
 })
+
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if (!task) {
+            res.status(404).send();
+        }
+        res.send(task);
+    } catch (error) {
+        res.status(500).send();
+    }
+})
+
 
 app.listen(port, () => {
     console.log('Server is up on port : ' + port);
