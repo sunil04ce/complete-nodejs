119201fb63b66993382b2921a47646c6714de935
sunil parmar <sunil04ce@gmail.com>
2023-07-26 23:44:43 +0530

    added middleware to save hashed password

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcrypt');
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -38,7 +39,19 @@ const User = mongoose.model('User', {
             }
         }
     }
+});
+
+userSchema.pre('save', async function (next) {
+    const user = this;
+    //console.log("Just before saving!");
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+    next()
 })
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User;
 
