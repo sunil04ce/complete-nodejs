be4d91d24e345d8ba0fdd18a14171bfb744a665a
sunil parmar <sunil04ce@gmail.com>
2023-07-30 18:34:53 +0530

    added  middleware

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -7,6 +7,19 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+    // console.log(req.method, req.path);
+    // next();
+
+    // if (req.method === 'GET') {
+    //     res.send('GET requests are disabled');
+    // } else {
+    //     next();
+    // }
+
+    res.status(503).send('Site is under maintenance.');
+});
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
