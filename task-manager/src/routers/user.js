e30a6c6d1765b5b108b5b7ae1959abdec9ee947f
sunil parmar <sunil04ce@gmail.com>
2023-07-31 23:00:07 +0530

    accepting authentication token

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = new express.Router();
+const auth = require('../middleware/auth');
 const User = require('../models/user');
 
 router.post('/users', async (req, res) => {
@@ -24,13 +25,17 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({});
-        res.send(users);
-    } catch (error) {
-        res.status(500).send();
-    }
+// router.get('/users', auth, async (req, res) => {
+//     try {
+//         const users = await User.find({});
+//         res.send(users);
+//     } catch (error) {
+//         res.status(500).send();
+//     }
+// })
+
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user);
 })
 
 router.get('/users/:id', async (req, res) => {
