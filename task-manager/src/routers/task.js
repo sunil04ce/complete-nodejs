a9c6647da25b18a6c8a96f028d5eefbf1146c73e
sunil parmar <sunil04ce@gmail.com>
2023-08-07 23:19:27 +0530

    paginating data

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -19,6 +19,7 @@ router.post('/tasks', auth, async (req, res) => {
 })
 
 // GET /tasks?completed=true
+// GET /tasks?limit=10&skip=0
 router.get('/tasks', auth, async (req, res) => {
     try {
         // const tasks = await Task.find({});
@@ -33,7 +34,11 @@ router.get('/tasks', auth, async (req, res) => {
 
         await req.user.populate({
             path: 'tasks',
-            match
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip)
+            }
         });
         res.send(req.user.tasks);
     } catch (error) {
