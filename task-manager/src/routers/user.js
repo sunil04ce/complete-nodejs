96180822b545f22145fa4944e6eb0006db9e1cf3
sunil parmar <sunil04ce@gmail.com>
2023-08-10 09:40:48 +0530

    added file validation for user avatar

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -125,7 +125,16 @@ router.delete('/users/me', auth, async (req, res) => {
 })
 
 const upload = multer({
-    dest: 'avatars'
+    dest: 'avatars',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload an image file'));
+        }
+        cb(undefined, true);
+    }
 
 });
 
