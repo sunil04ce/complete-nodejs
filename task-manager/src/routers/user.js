e4ab06b91e145b65906a97ef0afde0ce68270f08
sunil parmar <sunil04ce@gmail.com>
2023-08-16 22:43:50 +0530

    use of sharp, image auto cropping and formatting

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = new express.Router();
 const multer = require('multer');
+const sharp = require('sharp');
 const auth = require('../middleware/auth');
 const User = require('../models/user');
 
@@ -139,7 +140,9 @@ const upload = multer({
 });
 
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-    req.user.avatar = req.file.buffer;
+    // req.user.avatar = req.file.buffer;
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer();
+    req.user.avatar = buffer;
     await req.user.save();
     res.send();
 }, (error, req, res, next) => {
@@ -160,7 +163,7 @@ router.get('/users/:id/avatar', async (req, res) => {
             throw new Error();
         }
 
-        res.set('Content-Type', 'image/jpg');
+        res.set('Content-Type', 'image/png');
         res.send(user.avatar);
     } catch (error) {
         res.status(404).send();
