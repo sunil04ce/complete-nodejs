9efc55c7870b9088e3e917293801dea1558f2f09
sunil parmar <sunil04ce@gmail.com>
2023-09-02 21:45:10 +0530

    added socket.io events

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -12,8 +12,23 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection', () => {
+// let count = 0;
+
+io.on('connection', (socket) => {
     console.log('New WebSocket connection');
+
+    socket.emit('message', 'Welcome!');
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message);
+    })
+
+    // socket.emit('countUpdated', count); // emit for current socket connection
+    // socket.on('increment', () => {
+    //     count++;
+    //     // socket.emit('countUpdated', count);
+    //     io.emit('countUpdated', count); // emit for all socket connections
+    // })
 })
 
 server.listen(port, () => {
