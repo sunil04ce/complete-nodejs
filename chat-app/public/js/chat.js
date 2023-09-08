2ba23dfcb5328390739871f354dc1ed4bc6880f6
sunil parmar <sunil04ce@gmail.com>
2023-09-08 22:44:02 +0530

    rendering messages

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -6,9 +6,18 @@ const $messageFormInput = $messageForm.querySelector('input');
 const $messageFormButton = $messageForm.querySelector('button');
 
 const $sendLocationButton = document.querySelector('#send-location');
+const $messages = document.querySelector('#messages');
+
+//Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML;
+
 
 socket.on('message', (message) => {
     console.log(message);
+    const html = Mustache.render(messageTemplate, {
+        message
+    });
+    $messages.insertAdjacentHTML('beforeend', html);
 })
 
 $messageForm.addEventListener('submit', (e) => {
